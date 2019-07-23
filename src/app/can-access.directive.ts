f8425fc235c82ebe8724d436040c33aa9393aad6
DaiDH <40824445+id1945@users.noreply.github.com>
2019-07-23 21:56:55 +0700

    Update can-access.directive.ts

--- src/app/can-access.directive.ts
+++ src/app/can-access.directive.ts
@@ -1,10 +1,37 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, OnDestroy, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthorizedService } from '../services';
 
 @Directive({
   selector: '[appCanAccess]'
 })
-export class CanAccessDirective {
+export class CanAccessDirective implements OnInit, OnDestroy {
+  @Input()
+  public appCanAccess: string | string[];
+  private permission$: Subscription;
 
-  constructor() { }
+  constructor(private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+    private authService: AuthorizedService) { }
+
+  ngOnDestroy() {
+    this.permission$.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.applyPermission();
+  }
+
+  private applyPermission(): void {
+    this.permission$ = this.authService
+      .checkAuthorization(this.appCanAccess)
+      .subscribe((authorized: any) => {
+        if (authorized.code === '00') {
+          this.viewContainer.createEmbeddedView(this.templateRef);
+        } else {
+          this.viewContainer.clear();
+        }
+      });
+  }
 
 }
