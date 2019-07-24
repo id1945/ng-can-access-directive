c17666fc9f37899afcb549bff93172b2fba305f0
antonio <antonio@localhost.localdomain>
2019-07-23 21:54:32 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanAccessDirective } from './can-access.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanAccessDirective
   ],
   imports: [
     BrowserModule,
